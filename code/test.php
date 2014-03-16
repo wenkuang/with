14556168fdb6f3d0e066c18a00cb6baf7ec0a2b1
Kempe <569576618@qq.com>
2014-03-16 15:59:01 +0800

    auto commit in shell

--- code/test.php
+++ code/test.php
@@ -46,7 +46,6 @@ function confirm_recharge($orderid,$id){
         "id" => $id, //来自之前的API返回结果
         "orderid" => $orderid,
         "format" => "json",
-        
     );
     $url = oufei_config::$api_confirm . "?" . http_build_query($data);
     return get_api($url);
