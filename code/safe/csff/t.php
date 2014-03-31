4732ffe8985baacc74015b1d34b97452fceff88b
Kempe <569576618@qq.com>
2014-03-31 16:47:01 +0800

    auto commit in shell

--- code/safe/csff/t.php
+++ code/safe/csff/t.php
@@ -2,7 +2,7 @@
 
 <form action="http://sms.sina.com.cn/weibosms/sms_weibo/index.php/sms_set/new_sms_set/add_rec" method="post" id="f">
 
-<input type="hidden" name="nickname" value="参考消息" />
+<input type="hidden" name="nickname" value="淘宝网" />
 
 </form>
 
