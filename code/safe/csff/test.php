db916f2e7af23692a15c6ab1140ffd6b31102ec5
Kempe <569576618@qq.com>
2014-03-31 15:53:01 +0800

    auto commit in shell

--- code/safe/csff/test.php
+++ code/safe/csff/test.php
@@ -2,7 +2,7 @@
 <?php
 
 
-$danger_site_referer = "http://sms.sina.com.cn//134314/index.php";
+$danger_site_referer = "http://sms.sina.com.cn/134314/index.php";
 $referer ="http://www.baidu.com/dasd.php";$_SERVER['HTTP_REFERER'];
 
 $allowed_referer=  $_SERVER ['HTTP_HOST'];
@@ -14,12 +14,13 @@ class csrf{
     public static function check_referer($referer="",$domain = ""){
         #请求来源网址
         $referer =empty($referer)?$_SERVER['HTTP_REFERER']:$referer;
+        
         #当前服务器域名，
         $allowed_domain = empty($domain)?$_SERVER ['HTTP_HOST']:$domain;
         if(!empty($referer)){
             
             preg_match('@^(?:http://)?([^/]+)@i',"$referer", $matches);
-            
+
             if(!empty($matches[1]) && $matches[1] == $allowed_domain){
                 return true;
             }
