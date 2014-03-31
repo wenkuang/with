7fe2f72b38aab781caddcd3c408b90be3d2ebde0
Kempe <569576618@qq.com>
2014-03-31 15:40:01 +0800

    auto commit in shell

--- code/safe/csff/test.php
+++ code/safe/csff/test.php
@@ -3,13 +3,13 @@
 
 
 $danger_site_referer = "http://www.baidu.com";
-$referer =$_SERVER['HTTP_REFERER'];
+$referer ="http://www.1.wctest.sinaapp.com/csrfsina/shouquan.html";$_SERVER['HTTP_REFERER'];
 $allowed_referer=  $_SERVER ['HTTP_HOST'];
 echo $allowed_referer;
 $c = preg_match('@^(?:http://)?([^/]+)@i',
     "$referer", $matches);
 
-print_r($matchs);
+print_r($matches);
 
 
 
