4b2b879916185d29c806f8f2c40ece39cce907b8
Kempe <569576618@qq.com>
2014-03-31 15:31:01 +0800

    auto commit in shell

--- code/safe/csff/test.php
+++ code/safe/csff/test.php
@@ -6,6 +6,7 @@ $danger_site_referer = "http://www.baidu.com";
 $referer =$_SERVER['HTTP_REFERER'];
 $allowed_referer= "http://" . $_SERVER ['HTTP_HOST'];
 echo $allowed_referer;
+echo $referer;
 if(!empty($referer) ){
     
     
